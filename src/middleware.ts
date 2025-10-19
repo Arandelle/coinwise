4624e2e15eb10a1848a4bc705a97e44fecc4d89f
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-10-20 03:12:38 +0800

    update: dynamic routing

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest){
     const token = req.cookies.get("access_token")?.value;
 
-    const isProtectedRoute = req.nextUrl.pathname.startsWith("/dashboard")
-    const isAuthRoute = req.nextUrl.pathname.startsWith("/login")
+    const authRoutes = ["/login", "/signup"]
+    const protectedRoutes = ["/dashboard", "/logout"];
+
+    const isProtectedRoute = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
+    const isAuthRoute = authRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
 
     if (isProtectedRoute && !token){
         return NextResponse.redirect(new URL("/login", req.url))
@@ -18,5 +21,5 @@ export function middleware(req: NextRequest){
 }
 
 export const config = {
-    matcher: ["/dashboard/:path*", "/login", "/register"]
+    matcher: ["/dashboard/:path*","/logout/:path*", "/login", "/signup"]
 }
