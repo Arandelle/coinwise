def092a211f4a691a769405f72b4963fbac67827
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-10-21 02:14:13 +0800

    update: update the info, and remove unused imports

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,15 +1,11 @@
 // components/UserProfile.tsx
 'use client';
 
-import LoadingCoin from '@/app/components/Loading';
-import { useUser } from '../../hooks/useUser';
-import { useRouter } from 'next/navigation';
-
 export default function Dashboarad() {
 
   return (
     <div className="max-w-2xl mx-auto p-6">
-      HEllo
+      This dashboard page
     </div>
   );
 }
