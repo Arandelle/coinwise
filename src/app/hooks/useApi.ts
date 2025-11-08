7da8299ff1a565e8669480daba7d26cd8cff27f7
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-11-08 22:06:20 +0800

    feat: add other http method

--- src/app/hooks/useApi.ts
+++ src/app/hooks/useApi.ts
@@ -15,10 +15,66 @@ async function apiFetch(url: string, options?: RequestInit) {
 // ============================================
 // CATEGORIES
 // ============================================
-export function useCategories() {
+export function useGroupWithCategories() {
   return useQuery({
-    queryKey: ['categories'],
-    queryFn: () => apiFetch('/api/category'),
+    queryKey: ['group-with-categories'],
+    queryFn: () => apiFetch('/api/group-with-categories'),
     staleTime: 10 * 60 * 1000, // Categories rarely change, cache for 10 min
   });
+}
+
+export function useCategory(){
+    return useQuery({
+        queryKey: ['categories'],
+        queryFn: () => apiFetch('/api/categories'),
+        staleTime: 10 * 60 * 1000
+    })
+}
+
+export function useCreateCategory(){
+
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn : (newCategory : any) =>
+            apiFetch('/api/categories', {
+                method: 'POST',
+                headers: {'Content-Type' : 'application/json'},
+                body: JSON.stringify(newCategory)
+            }),
+        onSuccess: () => {
+            // invalidate and refetch categories
+            queryClient.invalidateQueries({queryKey: ['categories']});
+        }
+    });
+}
+
+export function useUpdateCategory(){
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: ({id, updates}: {id: string, updates: any}) => 
+            apiFetch(`/api/category/${id}`,{
+                method: 'PUT',
+                headers: {'Content-Type' : 'applicaton/json'},
+                body: JSON.stringify(updates)
+            }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['categories']});
+        },
+    });
+}
+
+export function useDeleteCategory(){
+    const queryClient = useQueryClient();
+
+    return useMutation({
+        mutationFn: (id: string) => 
+            apiFetch(`/api/categories/${id}`, {
+                method: "DELETE",
+            }),
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ["categories"]});
+        }
+    })
 }
