a618ff822e9ee92c6255ef04e54475c41f4bcba1
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-11-09 16:52:24 +0800

    move the group with category type

--- src/app/types/Category.ts
+++ src/app/types/Category.ts
@@ -4,8 +4,16 @@ export interface Category {
   type: string,
   icon: string;
   color?: string;
-  category_group: string;
+  group_id: string;
+
+  category_group?: string
 }
+
+export type CategoryGroup = {
+  _id: string;
+  group_name: string;
+  type: string
+};
 
 export type CreateCategory = Omit<Category, "_id">;
 export type UpdateCategoryInput = Partial<Omit<Category, "_id">>
