7ddd2679f5618dfcd9a138da4d47c1cb90387947
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-11-10 23:43:46 +0800

    feat: add the calendar component, in grid style

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,11 +1,15 @@
 // components/UserProfile.tsx
-'use client';
+"use client";
+
+import MiniCalendar from "@/app/components/MiniCalendar";
 
 export default function Dashboarad() {
-
+  
   return (
-    <div className="max-w-2xl mx-auto p-6">
-      This dashboard page
+    <div className="grid grid-cols-3 p-6 bg-slate-50 max-w-3xl mx-auto rounded-md">
+      <div className="mx-auto col-span-3">
+        <MiniCalendar />
+      </div>
     </div>
   );
 }
