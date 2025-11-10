064ffec0f88468dda383a4f007a6eaf676478387
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-11-11 01:58:49 +0800

    feat: update how the minicalender included, since styled of it was done on its component

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -6,10 +6,6 @@ import MiniCalendar from "@/app/components/MiniCalendar";
 export default function Dashboarad() {
   
   return (
-    <div className="grid grid-cols-3 p-6 bg-slate-50 max-w-3xl mx-auto rounded-md">
-      <div className="mx-auto col-span-3">
-        <MiniCalendar />
-      </div>
-    </div>
+    <MiniCalendar />
   );
 }
