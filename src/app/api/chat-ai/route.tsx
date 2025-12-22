079f36029fb8ec51d013061ace1c5a885c42cb55
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-12-22 23:09:21 +0800

    feat: add auth token

--- src/app/api/chat-ai/route.tsx
+++ src/app/api/chat-ai/route.tsx
@@ -1,13 +1,19 @@
 import { AIPrompt } from "@/app/types/AIPrompt";
+import { getToken } from "@/lib/getToken";
 
 export async function POST(req: Request){
     try{
 
         const body : AIPrompt = await req.json();
 
+        const token = await getToken();
+
         const response = await fetch(`${process.env.BACKEND_URL}/ai/coinwise-ai`, {
             method: "POST",
-            headers: {"Content-Type" : "application/json"},
+            headers: {
+                "Content-Type" : "application/json",
+                "Authorization" : `Bearer ${token}`
+            },
             body: JSON.stringify(body)
         });
 
