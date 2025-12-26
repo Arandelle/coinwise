7e8c16ae64867e6c816935725c927976c28817d0
Arandelle Paguinto <topaguintsarandell@gmail.com>
2025-12-27 01:28:59 +0800

    feat: rename from middleware to proxy, since middleware are depracted

---
+++ src/proxy.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function proxy(req: NextRequest){
+    const token = req.cookies.get("access_token")?.value;
+
+    const authRoutes = ["/login", "/signup"]
+    const protectedRoutes = ["/dashboard", "/transactions"];
+
+    const isProtectedRoute = protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
+    const isAuthRoute = authRoutes.some((route) => req.nextUrl.pathname.startsWith(route))
+    const isRootRoute = req.nextUrl.pathname === "/"
+
+    // Redirect authenticated users from root to dashboard
+    if (isRootRoute && token){
+        return NextResponse.redirect(new URL("/dashboard", req.url))
+    }
+
+    if (isProtectedRoute && !token){
+        return NextResponse.redirect(new URL("/login", req.url))
+    }
+
+    if (isAuthRoute && token){
+        return NextResponse.redirect(new URL("/dashboard", req.url))
+    }
+
+    return NextResponse.next()
+}
+
+export const config = {
+    matcher: ["/", "/dashboard/:path*","/transactions/:path*", "/login", "/signup"]
+}
